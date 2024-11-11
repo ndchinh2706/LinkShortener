8a7d9196516551938c2470a3aa7ca449e7fe7bb6
Nguyễn Đình Chính <nd.chysnh@gmail.com>
2024-11-11 17:24:40 +0700

    Thay tên đổi họ của nhà người ta

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,13 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  title: 'Đi uống bia đê',
+  email: '',
+  github: '',
+  twitter: '',
+  telegram: '',
+  mastodon: '',
+  blog: '',
+  description: 'Đi uống bia đê, uống bia đê uống bia đê',
+  image: '',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
