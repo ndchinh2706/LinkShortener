90168ec730ffecb2d9a4b4c7c819907f592e3acd
Nguyễn Đình Chính <nd.chysnh@gmail.com>
2024-11-11 19:32:11 +0700

    Update Hero.vue

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { AreaChart } from 'lucide-vue-next'
 import { GitHubIcon } from 'vue3-simple-icons'
-import heroImg from '@/assets/images/biahoi.png?raw'
 
 const { title, description, github } = useAppConfig()
 </script>
